d08fda281c3720f202750efc939a9ca138de6bea
Alfonso Jimenez <aljimeruz@gmail.com>
2025-06-12 12:54:11 +0200

    Add BaseLayout como plantilla base

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
 document.querySelector('.hamburger')?.addEventListener('click', () => {
     document.querySelector('.nav-links')?.classList.toggle('expanded');
 });
+
+function sumar() {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+}
